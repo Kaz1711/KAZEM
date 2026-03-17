5460e8bacd1089caf5964e9ebe7a687c755d4f40
Kazem <Kazem.mouchaimech@umontreal.ca>
2026-03-17 11:00:17 -0400

    save la figure

--- travail.jl
+++ travail.jl
@@ -144,7 +144,6 @@ end
 axislegend(ax)
 tightlimits!(ax)
 display(f)
-save("travail-1.png", f) # sauvegarder la figure pour une utilisation dans le rapport 
 
 # ## Verifications de l'équilibre
 function check_success(T, s)
